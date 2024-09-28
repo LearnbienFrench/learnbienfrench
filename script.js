0c345a283b5433ee855f5be04fa94e092fa73bc7
LearnbienFrench <learnbienfrench@gmail.com>
2024-09-28 17:58:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.getElementById('quizForm').addEventListener('submit', function(event) {
 
     // Correct answers for each question
     const correctAnswers = {
-        q1: 'B',
-        q2: 'C',
-        q3: 'B',
-        q4: 'C',
+        q1: 'A',
+        q2: 'B',
+        q3: 'C',
+        q4: 'D',
         q5: 'A'
     };
 
